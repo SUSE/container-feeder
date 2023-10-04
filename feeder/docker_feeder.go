7d1f8593a755a33779d65a8c23a550faa73ee9cc
Dan Čermák <dcermak@suse.com>
2023-10-04 16:02:15 +0200

    Don't subprocess to get the docker engine API version

--- feeder/docker_feeder.go
+++ feeder/docker_feeder.go
@@ -19,9 +19,9 @@ package feeder
 
 import (
 	"context"
+	"errors"
 	"io/ioutil"
 	"os"
-	"os/exec"
 	"strings"
 
 	"github.com/docker/docker/api/types"
@@ -48,21 +48,6 @@ func NewDockerFeeder() (*DockerFeeder, error) {
 	return feeder, nil
 }
 
-// dockerDaemonAPIVersion returns the API version supported by the server by
-// shelling out.
-func dockerDaemonAPIVersion() (string, error) {
-	out, err := exec.Command(
-		"docker",
-		"version",
-		"--format",
-		"{{.Server.APIVersion}}").Output()
-	if err != nil {
-		return "", err
-	}
-	api := strings.Trim(string(out[:]), "\n")
-	return api, nil
-}
-
 // connectToDaemon returns a Docker client.Client using the right version of
 // the API
 func connectToDaemon() (*client.Client, error) {
@@ -70,15 +55,36 @@ func connectToDaemon() (*client.Client, error) {
 	// try to use the latest one, which might be too new compared to the
 	// one supported by the docker daemon
 
-	apiVersion, err := dockerDaemonAPIVersion()
+	cli, err := client.NewEnvClient()
 	if err != nil {
 		return nil, err
 	}
+	vers, err := cli.ServerVersion(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	var apiVersion string = ""
+
+	for _, component := range vers.Components {
+		if component.Name != "Engine" {
+			continue
+		}
+		if vers, found := component.Details["ApiVersion"]; found {
+			apiVersion = vers
+		}
+	}
+
+	if apiVersion == "" {
+		return nil, errors.New("Could not obtain docker Engine API version")
+	}
+
 	if err := os.Setenv("DOCKER_API_VERSION", apiVersion); err != nil {
 		return nil, err
 	}
 
-	cli, err := client.NewEnvClient()
+	cli, err = client.NewEnvClient()
+
 	if err != nil {
 		return nil, err
 	}
